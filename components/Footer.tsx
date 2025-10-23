5cd104093cee51df9435cc6f42b4b815167640fa
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-10-23 16:42:28 +0530

    Implemented footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center text-slate-500 text-sm mt-12">
+      <p>Made with ❤️ by Royston in 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
