9a481e543146d25d87e878718a96404ae5b3c921
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-10-25 16:08:17 +0530

    Created VOICES constants with recognized google voice names

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const VOICES = [
+  { id: 'Kore', name: 'Kore (Female)' },
+  { id: 'Puck', name: 'Puck (Male)' },
+  { id: 'Zephyr', name: 'Zephyr (Female)' },
+  { id: 'Fenrir', name: 'Fenrir (Male)' },
+];
