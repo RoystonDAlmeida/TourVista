62fefddc82ab6ca5bed64aed540ccefd296f91c0
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-10-25 20:12:45 +0530

    Passing of discoveryId prop to ResultDisplay component

--- components/DiscoveryDetail.tsx
+++ components/DiscoveryDetail.tsx
@@ -77,12 +77,15 @@ const DiscoveryDetail = ({ user }: DiscoveryDetailProps) => {
   }
 
   return (
-    <ResultDisplay
-      user={user}
-      imageFile={null}
-      landmarkInfo={discovery.landmarkInfo}
-      imageUrl={discovery.imageUrl}
-    />
+    <div className="flex flex-col items-center w-full max-w-4xl mx-auto"> {/* Add a wrapper div */}
+      <ResultDisplay
+        user={user}
+        imageFile={null}
+        landmarkInfo={discovery.landmarkInfo}
+        imageUrl={discovery.imageUrl}
+        discoveryId={discoveryId!}
+      />
+      </div>
   );
 };
 
